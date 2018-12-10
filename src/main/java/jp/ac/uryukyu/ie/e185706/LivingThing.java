e5381b2260f215796e2cea04ff3c10f9fb29f0ea
Megumi Uza <mik.vanrgby@gmail.com>
2018-12-10 16:45:56 +0900

    step3 Wrote JavaDoc

--- src/main/java/jp/ac/uryukyu/ie/e185706/LivingThing.java
+++ src/main/java/jp/ac/uryukyu/ie/e185706/LivingThing.java
@@ -6,7 +6,16 @@ public class LivingThing {
     private int attack;
     private boolean dead;
 
-    //コンストラクタ
+    /**
+     *
+     * 任意のキャラクターの情報をLivingThingクラスの変数に入れ、
+     * 初期情報として出力する。
+     *
+     * @param name 使用する名前
+     * @param hitPoint　HP
+     * @param attack　攻撃力
+     *
+     */
     public LivingThing(String name,int hitPoint,int attack){
         this.name = name;
         this.hitPoint = hitPoint;
@@ -15,14 +24,34 @@ public class LivingThing {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
     }
 
+    /**
+     *
+     * LivingThingクラスで使われている変数deadを取得するためのメソッド。
+     *
+     * @return dead LivingThingクラスで使用されている変数
+     */
     public boolean isDead(){
         return dead;
     }
 
+
+    /**
+     *
+     * LivingThingクラスで使われている変数nameを取得するためのメソッド。
+     *
+     * @return name LivingThingクラスで使用されている変数
+     */
     public String getName(){
         return name;
     }
 
+    /**
+     *
+     * もしHero（Enemy）が死んでいないならば、相手に対して攻撃する。
+     *
+     * @param opponent　名前、HP、攻撃力の情報
+     *
+     */
     public void attack(LivingThing opponent){
 
         if(!dead) {
@@ -31,6 +60,14 @@ public class LivingThing {
             opponent.wounded(damage);
         }
     }
+
+    /**
+     *
+     * 与えたダメージをHPから引き、生存しているか確認する。
+     *
+     * @param damage　attackメソッドで攻撃して与えたダメージ
+     *
+     */
 
     public void wounded(int damage){
         hitPoint -= damage;
@@ -41,15 +78,32 @@ public class LivingThing {
 
     }
 
-
+    /**
+     *
+     * LivingThingクラスで使われている変数hitPointに任意の数を設定するメソッド。
+     *
+     * @param hitPoint　設定したいint型の数（HP）
+     */
     public void setHitPoint(int hitPoint) {
         this.hitPoint = hitPoint;
     }
 
+    /**
+     *
+     * LivingThingクラスで使われている変数hitPointを取得するためのメソッド。
+     *
+     * @return hitPoint LivingThingクラスで使用されている変数
+     */
     public int getHitPoint() {
         return hitPoint;
     }
 
+    /**
+     *
+     * LivingThingクラスで使われている変数deadに任意の真偽を設定するメソッド。
+     *
+     * @param dead　設定したいdeadの真偽
+     */
     public void setDead(boolean dead) {
         this.dead = dead;
     }
